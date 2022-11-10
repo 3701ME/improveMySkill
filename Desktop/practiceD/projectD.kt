5d8d756a508238316e2286c9276443aed9d767b0
3701ME <hunnykr@naver.com>
2022-11-10 16:38:12 +0900

    test2메세지

--- Desktop/practiceD/projectD.kt
+++ Desktop/practiceD/projectD.kt
@@ -2,6 +2,7 @@ fun main() {
     println("미션3 준비")
     println("프로젝트D에서 수정!")
     println("프로젝트E에서 수정함")
-    println("Fast-forward방식")
+    println("Fast-forward")
+    println("테스트2에서 추가한 내용입니다.")
 
 }
